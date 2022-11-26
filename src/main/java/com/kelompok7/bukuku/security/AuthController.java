688dbcef1b13184a5e629d72ef479835c8419b7b
FahtulIkhsan <fahtulikhsan@mail.ugm.ac.id>
2022-11-26 07:41:14 +0700

    minor auth

--- src/main/java/com/kelompok7/bukuku/security/AuthController.java
+++ src/main/java/com/kelompok7/bukuku/security/AuthController.java
@@ -1,10 +1,12 @@
 package com.kelompok7.bukuku.security;
 
 import com.kelompok7.bukuku.user.User;
+import com.kelompok7.bukuku.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,9 @@ import javax.mail.MessagingException;
 import java.io.File;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
@@ -22,6 +27,8 @@ import java.net.URI;
 public class AuthController {
     @Autowired
     private final AuthService authService;
+    @Autowired
+    private final UserService userService;
 
     @PostMapping("/register")
     public ResponseEntity<User> register(@RequestBody User user) throws MessagingException, UnsupportedEncodingException {
@@ -30,11 +37,21 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public String token(Authentication authentication){
+    public ResponseEntity<Map<String, String>> token(Authentication authentication){
+        Optional<User> user = Optional.ofNullable(userService.getUser(authentication.getName()));
+        if(!user.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        Map<String, String> map = new HashMap<String, String>();
+        if(!user.get().isEnabled()){
+            map.put("error", "Email belum diverifikasi");
+            return new ResponseEntity<>(map, HttpStatus.FORBIDDEN);
+        }
         log.info("Token requested for user: {}", authentication.getName());
         String token = authService.generateToken(authentication);
+        map.put("token", token);
         log.info("Token granted: {}", token);
-        return token;
+        return ResponseEntity.ok(map);
     }
     @GetMapping("/verify")
     public String verifyUser(@Param("code") String code) {
